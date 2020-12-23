00cf69e6f64aae75e73ca086aadc670be2b418e6
Matheus Nascimento <mnascimento0392@gmail.com>
2020-12-23 00:11:56 -0300

    itemWriter csv

--- src/main/java/mnascimento/api/Application.java
+++ src/main/java/mnascimento/api/Application.java
@@ -1,7 +1,5 @@
 package mnascimento.api;
 
-import java.util.List;
-
 import javax.sql.DataSource;
 
 import org.springframework.batch.core.Job;
@@ -12,14 +10,16 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.database.PagingQueryProvider;
-import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
 import org.springframework.batch.item.database.builder.JdbcPagingItemReaderBuilder;
 import org.springframework.batch.item.database.support.SqlPagingQueryProviderFactoryBean;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
+import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
+import org.springframework.core.io.FileSystemResource;
 
 import mnascimento.api.Domains.Order;
 
@@ -31,8 +31,8 @@ import mnascimento.api.Domains.Order;
 @EnableBatchProcessing
 public class Application {
 
-	public static String[] tokens = new String[] { "order_id", "first_name", "last_name", "email", "cost", "item_id",
-			"item_name", "ship_date" };
+	public static String[] tokens = new String[] { "orderId", "firstName", "lastName", "email", "cost", "itemId",
+			"itemName", "shipDate" };
 
 	public static String ORDER_SQL = "select order_id, first_name, last_name, "
 			+ "email, cost, item_id, item_name, ship_date " + "from SHIPPED_ORDER order by order_id";
@@ -46,40 +46,40 @@ public class Application {
 	@Autowired
 	public DataSource dataSource;
 
+	private ItemWriter<Order> ItemWriter() {
+		FlatFileItemWriter<Order> itemWriter = new FlatFileItemWriter<Order>();
+		itemWriter.setResource(new FileSystemResource("./data/shipped_out.csv"));
+		DelimitedLineAggregator<Order> aggregator = new DelimitedLineAggregator<Order>();
+		aggregator.setDelimiter(",");
+
+		BeanWrapperFieldExtractor<Order> fieldExtractor = new BeanWrapperFieldExtractor<Order>();
+		fieldExtractor.setNames(tokens);
+		aggregator.setFieldExtractor(fieldExtractor);
+		itemWriter.setLineAggregator(aggregator);
+		return itemWriter;
+	}
+
 	@Bean
 	public PagingQueryProvider queryProvider() throws Exception {
-		 SqlPagingQueryProviderFactoryBean factory= new SqlPagingQueryProviderFactoryBean();
+		SqlPagingQueryProviderFactoryBean factory = new SqlPagingQueryProviderFactoryBean();
 
-		 factory.setSelectClause("select order_id, first_name, last_name, email, cost, item_id, item_name, ship_date");
-		 factory.setFromClause("from SHIPPED_ORDER");
-		 factory.setSortKey("order_id");
-		 factory.setDataSource(dataSource);;
-		 return factory.getObject();
+		factory.setSelectClause("select order_id, first_name, last_name, email, cost, item_id, item_name, ship_date");
+		factory.setFromClause("from SHIPPED_ORDER");
+		factory.setSortKey("order_id");
+		factory.setDataSource(dataSource);
+		return factory.getObject();
 	}
 
 	@Bean
 	public ItemReader<Order> itemReader() throws Exception {
-		return new JdbcPagingItemReaderBuilder<Order>()
-				.dataSource(dataSource)
-				.name("JdbcCursorItemReader")
-				.queryProvider(queryProvider())
-				.rowMapper(new OrderRowMapper())
-				.pageSize(10)
-				.build();
+		return new JdbcPagingItemReaderBuilder<Order>().dataSource(dataSource).name("JdbcCursorItemReader")
+				.queryProvider(queryProvider()).rowMapper(new OrderRowMapper()).pageSize(10).build();
 	}
-
 
 	@Bean
 	public Step chunkBasedStep() throws Exception {
 		return this.stepBuilderFactory.get("chunkBasedStep").<Order, Order>chunk(10).reader(itemReader())
-				.writer(new ItemWriter<Order>() {
-
-					@Override
-					public void write(List<? extends Order> items) throws Exception {
-						System.out.println(String.format("Received list of size %s", items.size()));
-						items.forEach(System.out::println);
-					}
-				}).build();
+				.writer(ItemWriter()).build();
 	}
 
 	@Bean
