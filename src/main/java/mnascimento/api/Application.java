c75171f511a7c68db50cca6dc5c7a6a344cfbcd0
Matheus Nascimento <mnascimento0392@gmail.com>
2020-12-22 22:36:03 -0300

    itemReader csv

--- src/main/java/mnascimento/api/Application.java
+++ src/main/java/mnascimento/api/Application.java
@@ -17,12 +17,18 @@ import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
+
+import mnascimento.api.Domains.Order;
 
 /**
  * @author Matheus
@@ -32,6 +38,9 @@ import org.springframework.core.task.SimpleAsyncTaskExecutor;
 @EnableBatchProcessing
 public class Application {
 
+	public static String[] tokens = new String[] { "order_id", "first_name", "last_name", "email", "cost", "item_id",
+			"item_name", "ship_date" };
+
 	@Autowired
 	public JobBuilderFactory jobBuilderFactory;
 
@@ -39,17 +48,28 @@ public class Application {
 	public StepBuilderFactory stepBuilderFactory;
 
 	@Bean
-	public ItemReader<String> itemReader() {
-		return new SimpleItemReader();
+	public ItemReader<Order> itemReader() {
+		FlatFileItemReader<Order> itemReader = new FlatFileItemReader<Order>();
+		itemReader.setLinesToSkip(1);
+		itemReader.setResource(new FileSystemResource("./data/shipped_orders.csv"));
+
+		DefaultLineMapper<Order> lineMapper = new DefaultLineMapper<Order>();
+		DelimitedLineTokenizer tokenizer = new DelimitedLineTokenizer();
+		tokenizer.setNames(tokens);
+		lineMapper.setLineTokenizer(tokenizer);
+		lineMapper.setFieldSetMapper(new OrderFieldSetMapper());
+		itemReader.setLineMapper(lineMapper);
+
+		return itemReader;
 	}
 
 	@Bean
 	public Step chunkBasedStep() {
-		return this.stepBuilderFactory.get("chunkBasedStep").<String, String>chunk(3).reader(itemReader())
-				.writer(new ItemWriter<String>() {
+		return this.stepBuilderFactory.get("chunkBasedStep").<Order, Order>chunk(3).reader(itemReader())
+				.writer(new ItemWriter<Order>() {
 
 					@Override
-					public void write(List<? extends String> items) throws Exception {
+					public void write(List<? extends Order> items) throws Exception {
 						System.out.println(String.format("Received list of size %s", items.size()));
 						items.forEach(System.out::println);
 					}
@@ -58,8 +78,7 @@ public class Application {
 
 	@Bean
 	public Job job() {
-		return this.jobBuilderFactory.get("job")
-				.start(chunkBasedStep()).build();
+		return this.jobBuilderFactory.get("job").start(chunkBasedStep()).build();
 	}
 
 	public static void main(String[] args) {
